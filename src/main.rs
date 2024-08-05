4c8a971df2f5600ad8a0b7f81a106663b3f9d71f
Patrick Miller <paddymills@proton.me>
2024-08-05 12:16:31 -0400

    move templates and split member loads with htmx

--- src/main.rs
+++ src/main.rs
@@ -8,23 +8,33 @@ use std::sync::Arc;
 use tower_http::services::ServeDir;
 
 struct AppState {
-    env: Environment<'static>,
+    pages: Environment<'static>,
+    fragments: Environment<'static>,
 }
 
 // TODO: custom error type and remove the unrwaps
 impl AppState {
     fn new() -> Self {
-        let mut env = Environment::new();
+        let mut pages = Environment::new();
+        let mut fragments = Environment::new();
 
-        // load templates
-        env.add_template("base", include_str!("templates/base.jinja"))
+        // load page templates
+        pages
+            .add_template("base", include_str!("templates/pages/base.jinja"))
             .unwrap();
-        env.add_template("home", include_str!("templates/home.jinja"))
+        pages
+            .add_template("home", include_str!("templates/pages/home.jinja"))
             .unwrap();
-        env.add_template("members", include_str!("templates/members.jinja"))
+        pages
+            .add_template("members", include_str!("templates/pages/members.jinja"))
             .unwrap();
 
-        Self { env }
+        // load fragment templates
+        fragments
+            .add_template("members", include_str!("templates/fragments/members.jinja"))
+            .unwrap();
+
+        Self { pages, fragments }
     }
 }
 
@@ -67,6 +77,7 @@ async fn main() -> Result<(), std::io::Error> {
     let app = Router::new()
         .route("/", get(home))
         .route("/members", get(members))
+        .route("/get-members", get(members_data))
         .nest_service("/assets", ServeDir::new("assets"))
         .with_state(state);
 
@@ -79,7 +90,7 @@ async fn main() -> Result<(), std::io::Error> {
 }
 
 async fn home(State(state): State<Arc<AppState>>) -> Result<Html<String>, StatusCode> {
-    let template = state.env.get_template("home").unwrap();
+    let template = state.pages.get_template("home").unwrap();
 
     let rendered = template
         .render(context! {
@@ -92,14 +103,27 @@ async fn home(State(state): State<Arc<AppState>>) -> Result<Html<String>, Status
 }
 
 async fn members(State(state): State<Arc<AppState>>) -> Result<Html<String>, StatusCode> {
-    let template = state.env.get_template("members").unwrap();
+    let template = state.pages.get_template("members").unwrap();
 
     let rendered = template
         .render(context! {
             title => "Members",
+        })
+        .unwrap();
+
+    Ok(Html(rendered))
+}
+
+async fn members_data(State(state): State<Arc<AppState>>) -> Result<Html<String>, StatusCode> {
+    let template = state.fragments.get_template("members").unwrap();
+
+    let rendered = template
+        .render(context! {
             members => Member::load_data(),
         })
         .unwrap();
+
+    std::thread::sleep(std::time::Duration::from_secs(2));
 
     Ok(Html(rendered))
 }
