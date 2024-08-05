87fce5c5e9ae1a3eafda29a248cea4daf6220d7e
Patrick Miller <paddymills@proton.me>
2024-08-04 21:04:16 -0400

    add templates

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,29 @@
-use axum::{
-    body::Body,
-    http::{header, StatusCode},
-    response::Response,
-    routing::get,
-    Router,
-};
+use axum::extract::State;
+use axum::http::StatusCode;
+use axum::{response::Html, routing::get, Router};
+use minijinja::{context, Environment};
+use std::sync::Arc;
+
+use tower_http::services::ServeDir;
+
+struct AppState {
+    env: Environment<'static>,
+}
+
+// TODO: custom error type and remove the unrwaps
+impl AppState {
+    fn new() -> Self {
+        let mut env = Environment::new();
+
+        // load templates
+        env.add_template("base", include_str!("../templates/base.jinja"))
+            .unwrap();
+        env.add_template("home", include_str!("../templates/home.jinja"))
+            .unwrap();
+
+        Self { env }
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
@@ -19,7 +38,7 @@ async fn main() -> Result<(), std::io::Error> {
                 message
             ))
         })
-        .level(log::LevelFilter::Error)
+        // .level(log::LevelFilter::Error)
         .level_for("delaches", log::LevelFilter::Trace)
         .chain(
             fern::Dispatch::new()
@@ -38,8 +57,14 @@ async fn main() -> Result<(), std::io::Error> {
         .apply()
         .expect("failed to init logging");
 
+    // init state
+    let state = Arc::new(AppState::new());
+
     // build our application with a single route
-    let app = Router::new().route("/", get(index));
+    let app = Router::new()
+        .route("/", get(home))
+        .nest_service("/assets", ServeDir::new("assets"))
+        .with_state(state);
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -49,10 +74,15 @@ async fn main() -> Result<(), std::io::Error> {
     Ok(())
 }
 
-async fn index() -> Response {
-    Response::builder()
-        .status(StatusCode::OK)
-        .header(header::SERVER, "axum")
-        .body(Body::from("index page"))
-        .unwrap()
+async fn home(State(state): State<Arc<AppState>>) -> Result<Html<String>, StatusCode> {
+    let template = state.env.get_template("home").unwrap();
+
+    let rendered = template
+        .render(context! {
+            title => "Home",
+            welcome_text => "Hello World!",
+        })
+        .unwrap();
+
+    Ok(Html(rendered))
 }
