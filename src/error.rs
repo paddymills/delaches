17442c71a973c160168d4d53d8c002c28e7997f2
Patrick Miller <paddymills@proton.me>
2024-08-06 11:53:01 -0400

    refactor error

---
+++ src/error.rs
@@ -0,0 +1,35 @@
+use axum::http::StatusCode;
+use axum::response::{IntoResponse, Response};
+
+#[derive(Debug, thiserror::Error)]
+pub enum AppError {
+    #[error(transparent)]
+    IoError(#[from] std::io::Error),
+    #[error(transparent)]
+    LoggingError(#[from] log::SetLoggerError),
+    #[error(transparent)]
+    TemplateError(#[from] minijinja::Error),
+    #[error(transparent)]
+    SqliteError(#[from] sqlite::Error),
+    #[error(transparent)]
+    CsvError(#[from] csv::Error),
+    #[error("Requested resource not found")]
+    NotFound(String),
+}
+
+// Tell axum how to convert `AppError` into a response.
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        match self {
+            Self::NotFound(s) => (StatusCode::NOT_FOUND, s),
+            _ => (StatusCode::INTERNAL_SERVER_ERROR, self.to_string()),
+        }
+        .into_response()
+    }
+}
+
+impl Into<StatusCode> for AppError {
+    fn into(self) -> StatusCode {
+        self.into_response().status()
+    }
+}
