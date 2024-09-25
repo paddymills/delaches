beec56ca32532732551728a5c2c0f315b4102094
Patrick Miller <paddymills@proton.me>
2024-09-25 05:22:06 -0400

    add 404

--- members/main.py
+++ members/main.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 from . import db
 
 main = Blueprint('main', __name__)
@@ -10,3 +10,7 @@ def index():
 @main.route('/profile')
 def profile():
     return 'Profile'
+
+@app.errorhandler(404)
+def not_found(error):
+    resp = render_template('static/404.html'), 404
