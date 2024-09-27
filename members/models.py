5cc33bb943946b11814efc305f4e37797d942a89
Patrick Miller <paddymills@proton.me>
2024-09-27 13:06:58 -0400

    complete model defn

--- members/models.py
+++ members/models.py
@@ -1,6 +1,7 @@
 
 from . import db
 from flask_login import UserMixin
+from sqlalchemy import ForeignKey
 
 class User(UserMixin, db.Model):
 	__tablename__ = 'users'
@@ -18,11 +19,36 @@ class Member(db.Model):
 	__tablename__ = 'members'
 
 	id = db.Column(db.Integer, primary_key=True)
+	type = db.Column(db.String(8))
+	join_year = db.Column(db.Integer)
+	active = db.Column(db.Boolean)
+
 	first_name = db.Column(db.String(256))
 	last_name = db.Column(db.String(256))
+	birthdate = db.Column(db.Date)
+
+	address = db.Column(db.String(256))
+	city = db.Column(db.String(256))
+	state = db.Column(db.String(2))
+	zip = db.Column(db.String(5))
+
+	phone = db.Column(db.String(12))
+	email = db.Column(db.String(255))
 
 class Transaction(db.Model):
 	__tablename__ = 'transactions'
 
 	id = db.Column(db.Integer, primary_key=True)
-	timestamp = db.Column(db.DateTime, nullable=False)
+	member_id = db.Column(db.Integer, db.ForeignKey("members.id"))
+	member = db.relationship("Member", backref=db.backref("members", uselist=False))
+	
+	timestamp = db.Column(db.DateTime, server_default=db.func.now())
+	desc = db.Column(db.String(64))
+	amount = db.Column(db.Float)
+
+class Dues(db.Model):
+	__tablename__ = 'dues'
+
+	id = db.Column(db.Integer, primary_key=True)
+	desc = db.Column(db.String(64), nullable=True)
+	amount = db.Column(db.Float, nullable=True)
