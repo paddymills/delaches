278e2f1f73913b4cd4ff290de1fdf5e160ef15c5
Patrick Miller <paddymills@proton.me>
2024-09-27 15:24:49 -0400

    add data import

--- manage.py
+++ manage.py
@@ -1,6 +1,9 @@
 
-from members import create_app, db
+from argparse import ArgumentParser
+import polars
+
 from flask_migrate import upgrade, migrate, init, stamp
+from members import create_app, db
 from members.models import *
 
 def deploy():
@@ -13,7 +16,7 @@ def deploy():
     # TODO: cli to get pins
     db.session.add_all([
         User(pin=1234, user='bar'),
-        User(pin=5678, user='admin'),
+        User(pin=5678, user='admin', api_key='<api-key>'),
     ])
     db.session.commit()
 
@@ -23,4 +26,45 @@ def deploy():
     migrate()
     upgrade()
 
-deploy()
+def import_data(filename):
+    """Import data into the database."""
+
+    columns = {
+        "Mem#": "id",
+        "Type": "type",
+        "Last Name": "last_name",
+        "First Name": "first_name",
+        "Address": "address",
+        "City": "city",
+        "ST": "state",
+        "Zip ": "zip",
+        "Phone": "phone",
+        "e-mail": "email",
+        "Birthdate": "birthdate",
+        "Year Joined": "join_year",
+    }
+    data = polars.read_excel(source=filename, columns=list(columns.keys()))
+    data = data.rename(columns)
+    data = data.filter(data['first_name'].is_not_null())
+    # change birthdate to datetime
+    data = data.with_columns(polars.col('birthdate').str.to_date("%Y-%m-%d %H:%M:%S", strict=False))
+    print(data)
+
+    # dump data to the database
+    app = create_app()
+    app.app_context().push()
+    db.session.bulk_insert_mappings(Member, data.to_dicts())
+    db.session.commit()
+
+# ------------------------ Main entrypoint ------------------------
+parser = ArgumentParser()
+parser.add_argument('--deploy', action='store_true', help='Deploy the application database')
+parser.add_argument('--load', help='Import data into the application database')
+
+args = parser.parse_args()
+if args.deploy:
+    deploy()
+elif args.load:
+    import_data(args.load)
+else:
+    parser.print_help()
