6015f11d5bdc81e82c18a6132174e77fad8a3a05
Patrick Miller <paddymills@proton.me>
2024-09-28 00:58:08 -0400

    start member edit form

--- members/members.py
+++ members/members.py
@@ -19,13 +19,14 @@ def listing():
 @login_required
 def member(id):
 	logger.debug("requested member: {}".format(id))
-	return Member.query.get(id).as_dict()
+	
+	return render_template('member.html', member=Member.query.get(id))
 
 @members.put('/members/<id>')
 @login_required
 def add_member(id):
 	logger.debug("adding member: {}".format(id))
-	print(request.json)
+	print(request.form)
 	
 	return abort(501)
 
@@ -34,26 +35,31 @@ def add_member(id):
 def update_member(id):
 	logger.debug("updating member: {}".format(id))
 
-	match request.json['paymentType']:
-		case 'dues':
-			logger.debug("dues payment")
+	if request.json:
+		match request.json['paymentType']:
+			case 'dues':
+				logger.debug("dues payment")
 
-			transact = Transaction(member_id=id, desc='Dues', amount=request.json['amount'])
-			db.session.add(transact)
-			db.session.commit()
+				transact = Transaction(member_id=id, desc='Dues', amount=request.json['amount'])
+				db.session.add(transact)
+				db.session.commit()
 
-			return make_response({ "transactionId": transact.id }), 201
-		case 'fob':
-			logger.debug("fob payment")
+				return make_response({ "transactionId": transact.id }), 201
+			case 'fob':
+				logger.debug("fob payment")
 
-			transact = Transaction(member_id=id, desc='KeyFob', amount=request.json['amount'])
-			db.session.add(transact)
-			db.session.commit()
+				transact = Transaction(member_id=id, desc='KeyFob', amount=request.json['amount'])
+				db.session.add(transact)
+				db.session.commit()
 
-			return make_response({ "transactionId": transact.id }), 201
-		case _:
-			logger.debug("unknown payment type")
-			return abort(501)
+				return make_response({ "transactionId": transact.id }), 201
+			case _:
+				logger.debug("unknown payment type")
+
+	if request.form:
+		print(request.form)
+
+	return abort(501)
 
 @members.get('/members/transactions/<id>')
 @login_required
